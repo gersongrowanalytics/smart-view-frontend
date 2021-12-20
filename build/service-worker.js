a39a6bf88295ee74774d310b3cab3c1883ddb933
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-20 12:46:18 -0500

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5ed53dec63e8e3ea023779d3290704c.js"
+  "/precache-manifest.0dadc4bc720dd2947167e7caf6391c27.js"
 );
 
 self.addEventListener('message', (event) => {
